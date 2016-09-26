7859ab0096dfe28b7ff215e7735d743a6f9a8976
Euklyd <euklyd@users.noreply.github.com>
2016-09-26 16:05:54 -0700

    change the looping thing a bit. it also doesn't work all the time so uh

--- loop_bot.sh
+++ loop_bot.sh
@@ -2,6 +2,8 @@
 # loop_bot.sh
 
 python3.5 bot.py
-while [[ $? -ne 0 ]]; do
+ERROR=$?
+while [[ ${ERROR} -ne 0 ]]; do
+  echo "[$(date "+%Y-%m-%d %H:%M:%S")] Bot exited with error code ${ERROR}; restarting."
   python3.5 bot.py
 done
