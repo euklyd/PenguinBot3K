71e8c7e74c578340b4edc733fd3b2c8d766c2cc9
Euklyd <euklyd@users.noreply.github.com>
2016-10-09 23:20:19 -0700

    Accept a greater range of emoji characters.

--- plugins/Macro.py
+++ plugins/Macro.py
@@ -20,13 +20,33 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class Macro(Plugin):
     def activate(self):
         pass
 
-    @command("^emojify ([\u263a-\U0001f645]|(?:<:.*:\d*>)) (.*)", access=-1)
+    # @command("^emojify ([\u263a-\U0001f645]|(?:<:.*:\d*>)) (.*)", access=-1)
+    @command("^emojify ([\u2600-\u26FF\u2700-\u27BF\U0001F300-\U0001F5FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF]|(?:<:.*:\d*>)) (.*)", access=-1)
     def emojify(self, msg):
         """`emojify <emoji> <sentence>`: replace all spaces in <sentence> with <emoji>"""
+        """
+        0x1F600...0x1F64F, // Emoticons
+        0x1F300...0x1F5FF, // Misc Symbols and Pictographs
+        0x1F680...0x1F6FF, // Transport and Map
+        0x2600...0x26FF,   // Misc symbols
+        0x2700...0x27BF,   // Dingbats
+        0xFE00...0xFE0F    // Variation Selectors
+        """
+        logger.info(msg.arguments)
+        emoji = msg.arguments[0]
+        reply = msg.arguments[1].replace(' ', ' {} '.format(emoji))
+        reply = "<@!{}>: {}".format(msg.sender, reply)
+        logger.info(reply)
+        self.say(msg.channel, reply)
+        self.delete_message(msg)
+
+    @command("^emojify-full \"(.|(?:<:.*:\d*>))\" (.*)", access=-1)
+    def emojify_full(self, msg):
         logger.info(msg.arguments)
         emoji = msg.arguments[0]
         reply = msg.arguments[1].replace(' ', ' {} '.format(emoji))
