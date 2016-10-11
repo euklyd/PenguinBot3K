1693f6bda5ab4c7a0eacfc83589877e16271b021
Euklyd <euklyd@users.noreply.github.com>
2016-10-10 21:49:00 -0700

    don't want everyone to be able to spam this lol

--- plugins/Nimious.py
+++ plugins/Nimious.py
@@ -33,7 +33,7 @@ class Nimious(Plugin):
         self.say(msg.channel, "^ Stryker when he {}".format(msg.arguments[0]))
         self.delete_message(msg)
 
-    @command("^(?:[Nn]imious)|(?:<@!?163167281042423810>)$", access=-1)
+    @command("^(?:[Nn]imious)|(?:<@!?163167281042423810>)$", access=100)
     def nimious(self, msg):
         dank_meme = "Shut the fuck up :ww_link_ugh: Nimious :ww_link_ugh:. You're :zzz: fraudulent. You can't do anything besides :bomb~1: spam :bow~1:. That's what you have :cancer: Rosa :star2: as a :second_place: secondary :second_place: because she complements your :cartwheel: roll habits :cartwheel: you cocky :toilet: ass low level smasher :toilet:. Gtfoh bruh. Your entire play style is :regional_indicator_z: 2 buttons :b:. You give Link mains a bad name. The reason no one cheers for you is because of the :cheese: fraudulence :cheese: you show in your play. A real Link main like :muscle: Izaw :muscle: gets my respect.\n:dank_link: You literally aren't shit dude :dank_link:"
         self.say(msg.channel, dank_meme)
