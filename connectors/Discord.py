e5bfe4dc8364fabfeaf18906eba6dd67c1422901
Euklyd <euklyd@users.noreply.github.com>
2016-10-28 20:04:27 -0700

    more legacy code deletion

--- connectors/Discord.py
+++ connectors/Discord.py
@@ -41,23 +41,23 @@ class Discord(Connector):
         self.name   = __name__
 
         # Authentication / Connection Data
-        self.connectorCache = {
-            "heartbeat_interval": 41250,
-            "session_id":"",
-            "self":{},
-            "private_channels":[],
-            "guilds":[]
-        }
+        # self.connectorCache = {
+        #     "heartbeat_interval": 41250,
+        #     "session_id":"",
+        #     "self":{},
+        #     "private_channels":[],
+        #     "guilds":[]
+        # }
 
         self.connected = False              # Boolean for handling connection state
-        self.token     = token              # Token used to authenticate api requests
-        self.socket    = None               # Websocket connection handler to Discord
+        # self.token     = token              # Token used to authenticate api requests
+        # self.socket    = None               # Websocket connection handler to Discord
 
-        self.auth_headers = {"authorization": self.token}
+        # self.auth_headers = {"authorization": self.token}
 
         # Internal threads
-        self.keep_alive_thread = None
-        self.message_consumer_thread = None
+        # self.keep_alive_thread = None
+        # self.message_consumer_thread = None
 
         # Events that this connector publishes
         self.core.event.register("connect")
@@ -70,33 +70,33 @@ class Discord(Connector):
         self.logger.info("Attempting connection to Discord servers")
 
         # Request a WebSocket URL
-        socket_url = self.request("GET", "gateway", headers=self.auth_headers)["url"]
+        # socket_url = self.request("GET", "gateway", headers=self.auth_headers)["url"]
 
-        self.socket = websocket.create_connection(socket_url)
+        # self.socket = websocket.create_connection(socket_url)
 
         # Immediately pass message to server about your connection
-        initData = {
-            "op": 2,
-            "d": {
-                "token": self.token,
-                "properties": {
-                    "$os": system(),
-                    "$browser":"",
-                    "$device":"Python",
-                    "$referrer":"",
-                    "$referring_domain":""
-                },
-            },
-            "v": 4
-        }
-        self._write_socket(initData)
+        # initData = {
+        #     "op": 2,
+        #     "d": {
+        #         "token": self.token,
+        #         "properties": {
+        #             "$os": system(),
+        #             "$browser":"",
+        #             "$device":"Python",
+        #             "$referrer":"",
+        #             "$referring_domain":""
+        #         },
+        #     },
+        #     "v": 4
+        # }
+        # self._write_socket(initData)
 
-        login_data = self._read_socket()
-        self.connectorCache['heartbeat_interval'] = login_data['d']['heartbeat_interval']
-        self.connectorCache['session_id']         = login_data['d']['session_id']
-        self.connectorCache['self']               = login_data['d']['user']
-        self.connectorCache['private_channels']   = login_data['d']['private_channels']
-        self.connectorCache['guilds']             = login_data['d']['guilds']
+        # login_data = self._read_socket()
+        # self.connectorCache['heartbeat_interval'] = login_data['d']['heartbeat_interval']
+        # self.connectorCache['session_id']         = login_data['d']['session_id']
+        # self.connectorCache['self']               = login_data['d']['user']
+        # self.connectorCache['private_channels']   = login_data['d']['private_channels']
+        # self.connectorCache['guilds']             = login_data['d']['guilds']
 
         self.connected = True
 
@@ -145,148 +145,13 @@ class Discord(Connector):
         except:
             self.logger.warning('Retrieval of messages in CID \'{}\' failed'.format(channel.id))
 
-    # def delete_message(self, msg):
-    #     self.logger.debug("Deleting message with ID: {} from channel: {}".format(msg.id, msg.channel))
+    # def gatherFacts(self):
     #
-    #     endpoint = "channels/{0}/messages/{1}".format(msg.channel, msg.id)
-    #     try:
-    #         self.request("DELETE", endpoint, headers=self.auth_headers)
-    #     except:
-    #         self.logger.warning('Deletion of message \'{}\' in channel \'{}\' failed'.format(msg.id, msg.channel))
+    #     self.connectorDict['self'] = self.request("GET", "users/@me", headers={"authorization": self.token})
+    #     self.connectorDict['direct_messages'] = self.request("GET", "users/@me/channels", headers={"authorization": self.token})
+    #     self.connectorDict['guilds'] = self.request("GET", "users/@me/guilds", headers={"authorization": self.token})
     #
-    # def delete_messages(self, msg_array):
-    #     self.logger.debug("Bulk deleting {} messages from channel: {}".format(len(msg_array), msg_array[0].channel))
-    #
-    #     endpoint = "channels/{0}/messages/bulk-delete".format(msg_array[0].channel)
-    #     data     = {
-    #         'messages': []
-    #     }
-    #     for msg in msg_array:
-    #         data['messages'].append(msg.message['id'])
-    #     # self.logger.info(data)
-    #     # self.logger.info(endpoint)
-    #     try:
-    #         self.request("POST", endpoint, data=data, headers=self.auth_headers)
-    #     except:
-    #         self.logger.warning('Deletion of message \'{}\' in channel \'{}\' failed'.format(msg.id, msg.channel))
-
-    # def ban_user(self, server_id, user_id, delete_msgs=0):
-    #     self.logger.info("Banning user `{}` from server `{}`".format(user_id, server_id))
-    #
-    #     endpoint = "guilds/{0}/bans/{1}".format(server_id, user_id)
-    #     data     = {
-    #         "delete-message-days": delete_msgs
-    #     }
-    #     self.logger.debug(endpoint)
-    #     self.logger.debug(data)
-    #
-    #     try:
-    #         self.request("PUT", endpoint, data=data, headers=self.auth_headers)
-    #     except:
-    #         self.logger.warning('Ban of user \'{}\' in server \'{}\' failed'.format(user_id, server_id))
-
-    # def whisper(self, user, message):
-    #     self.logger.debug("Sending reply to " + user)
-    #
-    #     channel = self.get_private_channel(user)
-    #     endpoint = "channels/{}/messages".format(channel)
-    #     data     = {
-    #         "content": "{}".format(message)
-    #     }
-    #
-    #     try:
-    #         self.request("POST", endpoint, data=data, headers=self.auth_headers)
-    #     except:
-    #         self.logger.warning('Reply to user \'{}\' in channel \'{}\' failed'.format(user, channel))
-
-    # def upload(self, channel, file):
-    #     self.logger.debug('Sending file to channel ' + channel)
-    #
-    #     endpoint = "channels/{}/messages".format(channel)
-    #     files    = {'file': open(file, 'rb')}
-    #
-    #     try:
-    #         self.request('POST', endpoint,  files=files, headers={"authorization": self.token})
-    #     except:
-    #         self.logger.warning('Upload file \'{}\' to channel {} failed'.format(file, channel))
-
-    # def getUsers(self):
-    #     pass
-    #
-    # @ttl_cache(300)
-    # def getUser(self, userID):
-    #     user = self.request("GET", "users/{}".format(userID), headers={"authorization": self.token})
-    #
-    #     return {
-    #         'user': user,
-    #         'name': user['username'],
-    #         'id': user['id'],
-    #         'expires': time.time() + 600
-    #     }
-
-    # def get_channel(self, msg):
-    #     channel = self.request("GET", "channels/{}".format(msg.channel), headers={"authorization": self.token})
-    #     return channel
-    #
-    # def get_server(self, msg):
-    #     channel = self.get_channel(msg)
-    #     server = self.request("GET", "guilds/{}".format(channel['guild_id']), headers={"authorization": self.token})
-    #     return server
-
-    # Discord Specific
-    # def leave_guild(self, guild_id):
-    #     self.request("DELETE", "users/@me/guilds/{}".format(guild_id), headers={"authorization": self.token})
-    #
-    # def get_servers(self):
-    #     return self.request("GET", "users/@me/guilds", headers={"authorization": self.token})
-    #
-    # def get_private_channel(self, user):
-    #     for channel in self.connectorCache['private_channels']:
-    #         if channel['recipient']['id'] == user:
-    #             return channel['id']
-    #     else:
-    #         channel = self.request("POST", "users/@me/channels", data={"recipient_id": "{}".format(user)}, headers={"authorization": self.token})
-    #         self.connectorCache['private_channels'].append(channel)
-    #         return channel['id']
-
-    # def set_status(self, status):
-    #     self._write_socket({
-    #         "op":3,
-    #         "d":{
-    #             "idle_since":None,
-    #             "game": {
-    #                 "name": status
-    #             }
-    #         }
-    #     })
-
-    # User Management
-        # Set roles
-        # Ban
-        # Sync Roles
-    # Channel Management
-        # Add / remove Channel
-        # Apply premissions to Channel
-
-    def gatherFacts(self):
-
-        self.connectorDict['self'] = self.request("GET", "users/@me", headers={"authorization": self.token})
-        self.connectorDict['direct_messages'] = self.request("GET", "users/@me/channels", headers={"authorization": self.token})
-        self.connectorDict['guilds'] = self.request("GET", "users/@me/guilds", headers={"authorization": self.token})
-
-        print(json.dumps(self.connectorDict))
-
-    # def avatar(self):
-    #     with open("conf/avatar.png", "rb") as avatarImage:
-    #         rawImage = avatarImage.read()
-    #
-    #     raw = base64.b64encode(rawImage)
-    #     with open("test", "wb") as file:
-    #         file.write(raw)
-    #
-    #     return
-    #
-    #     self.request("PATCH", "users/@me", data={"username": "Arcbot", "avatar": "data:image/png;base64," + base64.b64encode(rawImage).decode('ascii')}, headers={"authorization": self.token})
+    #     print(json.dumps(self.connectorDict))
 
     # Handler Methods
     async def _handleMessage(self, message):
