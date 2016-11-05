cdd9ad7e79aa90126664da27dd49bc5b6546a0db
Euklyd <euklyd@users.noreply.github.com>
2016-11-05 03:46:58 -0700

    Moved `repeat` to Moderator and added user info commands

--- plugins/Utility.py
+++ plugins/Utility.py
@@ -1,9 +1,10 @@
 """
     Plugin Name : Utility
-    Plugin Version : 1.1
+    Plugin Version : 2.1
 
     Description:
-        Provides basic utility commands, e.g., coin flips.
+        Provides basic utility commands, e.g., random selectors
+        and user info.
 
     Contributors:
         - Popguin / Euklyd
@@ -16,6 +17,7 @@
 
 from core.Plugin import Plugin
 from core.Decorators import *
+import discord
 import logging
 import random
 
@@ -23,20 +25,55 @@ logger = logging.getLogger(__name__)
 
 
 class Utility(Plugin):
-    def activate(self):
+    async def activate(self):
         pass
 
-    @command("^pick-one (.*)", access=-1)
-    def pick_one(self, msg):
-        """`pick-one <list of items>`: Selects one item out of a list of space-separated items."""
-        args = msg.arguments[0]
-        items = args.split(' ')
+    @command("^pick(?:[- ]one)? (.*)", access=-1)
+    async def pick_one(self, msg, arguments):
+        """`pick one <list of items>`: Selects one item out of a list of space-separated items."""
+        # args = arguments[0]
+        # items = args.split(' ')
+        items = arguments[0].split(' ')
         choice = random.choice(items)
-        self.say(msg.channel, "<@!{}>, your selection is **{}**!".format(msg.sender, choice))
+        await self.send_message(
+            msg.channel,
+            "<@!{}>, your selection is **{}**!".format(msg.author.id, choice)
+        )
 
+    @command("^avatar <@!?([0-9]*)>$", access=-1)
+    async def get_avatar(self, msg, arguments):
+        """`avatar @<user>`: posts a link to `<user>`'s avatar'"""
+        user = await self.core.get_user_info(arguments[0])
+        reply = "**Avatar for {}#{}:**\n{}".format(
+            user.name,
+            user.discriminator,
+            user.avatar_url
+        )
+        await self.send_message(msg.channel, reply)
 
-    @command("^repeat (.*)", access=900)
-    def repeat(self, msg):
-        """`repeat <message>`: Sends `<message>` to the channel anonymously."""
-        self.say(msg.channel, msg.arguments[0])
-        self.delete_message(msg)
+    @command("^info <@!?([0-9]*)>$", access=50)
+    async def get_info(self, msg, arguments):
+        user = msg.server.get_member(arguments[0])
+        # user = await self.core.get_member(arguments[0])
+        if (user is None):
+            user = await self.core.get_user_info(arguments[0])
+            reply =  "**Info for {}#{}:**".format(user.name, user.discriminator)
+            reply += "\n\t`ID:          `{}".format(user.id)
+            reply += "\n\t`Avatar:      `{}".format(user.avatar_url)
+            reply += "\n\t`Created:     `{}".format(user.created_at)
+        else:
+            reply =  "**Info for {}#{}:**".format(user.name, user.discriminator)
+            if (user.nick != user.name):
+                reply += "\n\t`Nick:        `{}".format(user.nick)
+            reply += "\n\t`ID:          `{}".format(user.id)
+            reply += "\n\t`Avatar:      `{}".format(user.avatar_url)
+            reply += "\n\t`Created:     `{}".format(user.created_at)
+            reply += "\n\t`Joined:      `{}".format(user.joined_at)
+            if (user.color != discord.Colour.default):
+                reply += "\n\t`Color:       `{} (`{}`)".format(
+                    user.color.to_tuple(), str(user.color)
+                )
+            reply += "\n\t`Role:        `{}".format(user.top_role)
+        if (user.bot is True):
+            reply += "\n\t`Bot account: `True".format(user.id)
+        await self.send_message(msg.channel, reply)
