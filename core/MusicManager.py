32b50db80bf2734f2ab14253dbe1a85e516ab939
Euklyd <euklyd@users.noreply.github.com>
2016-11-18 00:41:21 -0800

    load() needs to be a method of PlaylistEntry orz

--- core/MusicManager.py
+++ core/MusicManager.py
@@ -54,7 +54,7 @@ class PlaylistEntry():
         self.announcement = None
         self.yt_song = yt_song
 
-    async def load(voice, after):
+    async def load(self, voice, after=None):
         try:
             self.player = await voice.create_ytdl_player(
                 self.yt_song.url, after=after
@@ -160,8 +160,7 @@ class MusicManager():
                 self.current_song = self.yt_queue.get()
                 self.logger.debug("got next song")
                 announcement = await self.current_song.load(
-                    # voice=self.voice, after=self.advance_queue
-                    self.voice, self.advance_queue
+                    voice=self.voice, after=self.advance_queue
                 )
                 await self.core.send_message(
                     self.current_song.yt_song.channel,
