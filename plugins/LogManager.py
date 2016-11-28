1cc79d10ee2b5504ff99e6975c0175ea4f6cb8cf
Euklyd <euklyd@users.noreply.github.com>
2016-11-28 06:21:10 -0800

    Allow for requesting logs from a different channel

--- plugins/LogManager.py
+++ plugins/LogManager.py
@@ -35,6 +35,30 @@ class LogManager(Plugin):
         self.log_manager = self.core.log_manager
         pass
 
+    @command("^gimme logs <#([0-9]+)>(?: ([0-9]+))?$", access=ACCESS['retrieve'],
+             doc_brief="`gimme logs #<channel>`: Retrieve log files for <channel>.",
+             doc_detail=("`gimme logs #<channel>`: Uploads the current log "
+                         "file for the specified channel privately to the "
+                         "requestor.\n\t"
+                         "**Optional:** Specify a number after `gimme logs` to "
+                         "request that many days in the past in addition to "
+                         "today's file."))
+    async def remote_log_request(self, msg, arguments):
+        channel = self.core.get_channel(arguments[0])
+        if (len(arguments) == 2 and arguments[1] is not None):
+            if (int(arguments[1]) < 0):
+                await self.send_message(
+                    msg.author,
+                    ("Invalid argument for `gimme logs`: needs to be"
+                     "greater than or equal to 0.")
+                )
+                return
+            else:
+                days = int(arguments[1])
+        else:
+            days = 0
+        await self.send_logs(channel, msg.author, days=days, request_id=msg.id)
+
     @command("^gimme logs(?: ([0-9]+))?$", access=ACCESS['retrieve'],
              doc_brief="`gimme logs`: Retrieve log files for the curent channel.",
              doc_detail=("`gimme logs`: Uploads the current log file for the "
@@ -42,8 +66,7 @@ class LogManager(Plugin):
                          "**Optional:** Specify a number after `gimme logs` to "
                          "request that many days in the past in addition to "
                          "today's file."))
-    async def send_logs(self, msg, arguments):
-        print(arguments)
+    async def log_request(self, msg, arguments):
         if (len(arguments) == 1 and arguments[0] is not None):
             if (int(arguments[0]) < 0):
                 await self.send_message(
@@ -56,29 +79,38 @@ class LogManager(Plugin):
                 days = int(arguments[0])
         else:
             days = 0
-        filenames = self.log_manager.get_logs(msg.channel, days)
+        await self.send_logs(msg.channel, msg.author, days=days, request_id=msg.id)
+        await self.delete_message(msg)
+
+    async def send_logs(self, channel, requestor, days=0, request_id="0000"):
+        filenames = self.log_manager.get_logs(channel, days)
         if (len(filenames) > 1):
-            await self.send_zip(msg, filenames)
-        else:
+            await self.send_zip(channel, requestor, filenames, request_id=request_id)
+        elif (len(filenames) == 1):
             with open(filenames[0], 'rb') as fp:
                 await self.send_file(
-                    msg.author,
+                    requestor,
                     fp,
                     filename=filenames[0].split('/')[-1]
                 )
+        else:
+            await self.send_message(
+                requestor,
+                "ERROR: There are no logs to retrieve."
+            )
 
-    async def send_zip(self, msg, filenames):
+    async def send_zip(self, channel, destination, filenames, request_id="0000"):
         """
         Helper function for send_logs()
         """
         zip_name = "{ch_name}-{msg_id}.zip".format(
-            ch_name=msg.channel.name,
-            msg_id=msg.id
+            ch_name=channel.name,
+            msg_id=request_id
         )
         zip_path = "resources/{}".format(zip_name)
         with zipfile.PyZipFile(zip_path, mode='a') as log_zip:
             for logfile in filenames:
                 log_zip.write(logfile)
         with open(zip_path, 'rb') as log_zip:
-            await self.send_file(msg.author, log_zip, filename=zip_name)
+            await self.send_file(destination, log_zip, filename=zip_name)
         os.remove(zip_path)
