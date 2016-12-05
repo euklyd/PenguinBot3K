fb9889956c54c8f20b9ea31b3496ac8875c720fd
Euklyd <euklyd@users.noreply.github.com>
2016-12-04 21:56:20 -0800

    misc cleanup

--- plugins/Manage.py
+++ plugins/Manage.py
@@ -1,6 +1,6 @@
 """
     Plugin Name : Manage
-    Plugin Version : 3.1
+    Plugin Version : 3.1.1
 
     Description:
         Gives basic commands to the bot to manage and examine itself.
@@ -46,25 +46,45 @@ class Manage(Plugin):
     async def ping(self, msg, arguments):
         await self.send_message(msg.channel, "Pong")
 
+    @command("^source|git|github$", access=ACCESS['source'], name='source',
+             doc_brief="`source`: link to my github repository.")
+    async def source(self, msg, arguments):
+        await self.send_message(msg.channel, self.core.config.repo)
+
     @command("^trigger$", access=ACCESS['trigger'], name='trigger',
-             doc_brief=("`trigger`: prints the default prefix used to tell "
-             "me that you're using a command."))
+             doc_brief="`trigger`: prints the default prefix used to tell "
+             "me that you're using a command.")
     async def trigger(self, msg, arguments):
-        if (type(self.core.config.trigger) is tuple or type(self.core.config.trigger) is list):
-            await self.send_message(msg.channel, "My default trigger is `{}`".format(self.core.config.trigger[0]))
+        if (type(self.core.config.trigger) is tuple or
+                type(self.core.config.trigger) is list):
+            await self.send_message(
+                msg.channel, "My default trigger is `{}`".format(
+                    self.core.config.trigger[0])
+            )
         else:
-            await self.send_message(msg.channel, "My default trigger is `{}`".format(self.core.config.trigger))
+            await self.send_message(
+                msg.channel,
+                "My default trigger is `{}`".format(self.core.config.trigger)
+            )
 
     @command("^triggers$", access=ACCESS['trigger'], name='triggers',
-             doc_brief=("`triggers`: prints the prefixes used to tell me that "
-             "you're using a command, if there exist multiple."))
+             doc_brief="`triggers`: prints the prefixes used to tell me that "
+             "you're using a command, if there exist multiple.")
     async def triggers(self, msg, arguments):
-        if (type(self.core.config.trigger) is tuple or type(self.core.config.trigger) is list):
-            await self.send_message(msg.channel, "My default triggers are `{}`".format(self.core.config.trigger))
+        if (type(self.core.config.trigger) is tuple or
+                type(self.core.config.trigger) is list):
+            await self.send_message(
+                msg.channel,
+                "My default triggers are `{}`".format(self.core.config.trigger)
+            )
         else:
-            await self.send_message(msg.channel, "My default trigger is `{}`".format(self.core.config.trigger))
+            await self.send_message(
+                msg.channel,
+                "My default trigger is `{}`".format(self.core.config.trigger)
+            )
 
-    @command("^list plugins$", access=ACCESS["plugin_list"], name='list plugins',
+    @command("^list plugins$", access=ACCESS["plugin_list"],
+             name='list plugins',
              doc_brief="`list plugins`: lists all of my plugins.")
     async def list_plugins(self, msg, arguments):
         plugins = self.core.plugin.list()
@@ -132,17 +152,17 @@ class Manage(Plugin):
             # self.logger.info(command_list)
             # for cmd in command_list.values():
             #     self.logger.info(cmd.name)
-            command_block = "**List of commands in plugin `{}`:**\n".format(plugin)
-            for command in sorted(command_list.values(), key=lambda cmd: str(cmd.name)):
+            command_block = "**List of commands in plugin `{}`:**\n".format(
+                plugin
+            )
+            for command in sorted(
+                    command_list.values(),
+                    key=lambda cmd: str(cmd.name)
+            ):
                 if (plugin == command.plugin and command.access <= access):
                     blurb = command.doc_brief
                     if (blurb is not None):
                         command_block += blurb + '\n'
-            # for command in sorted(command_list.keys()):
-            #     if (plugin == command.split('.')[0] and command_list[command].access <= access):
-            #         docstr = command_list[command].callback.__doc__
-            #         if (docstr is not None and command_list[command].access <= access):
-            #             command_block += docstr + '\n'
             if (command_block.count('\n') > 1):
                 await self.send_message(msg.channel, command_block)
 
@@ -153,10 +173,10 @@ class Manage(Plugin):
 
     @command("^(enable|disable|reload|status) plugin ([A-Za-z]+)$",
              access=ACCESS["plugin_manage"], name='toggle plugin',
-             doc_brief=("`enable plugin <plugin>`: enables `<plugin>`.\n"
-                        "`disable plugin <plugin>`: disables `<plugin>`.\n"
-                        "`reload plugin <plugin>`: reloads `<plugin>`.\n"
-                        "`status plugin <plugin>`: fetches the status of `<plugin>`."))
+             doc_brief="`enable plugin <plugin>`: enables `<plugin>`.\n"
+             "`disable plugin <plugin>`: disables `<plugin>`.\n"
+             "`reload plugin <plugin>`: reloads `<plugin>`.\n"
+             "`status plugin <plugin>`: fetches the status of `<plugin>`.")
     async def manage_plugin(self, msg, arguments):
         plugins = self.core.plugin.list()
         plugin = arguments[1]
@@ -166,30 +186,39 @@ class Manage(Plugin):
             if action == "enable":
                 if plugins[plugin]['status'] == "Disabled":
                     self.core.plugin.load(plugin)
-                    await self.send_message(msg.channel, "Enabled plugin: **{}**".format(plugin))
+                    await self.send_message(
+                        msg.channel, "Enabled plugin: **{}**".format(plugin)
+                    )
                 else:
-                    await self.send_message(msg.channel, "{} is already enabled".format(plugin))
+                    await self.send_message(
+                        msg.channel, "{} is already enabled".format(plugin)
+                    )
 
             elif action == "disable":
                 if plugins[plugin]['status'] == "Enabled":
                     self.core.plugin.unload(plugin)
-                    await self.send_message(msg.channel, "Disabled plugin: **{}**".format(plugin))
+                    await self.send_message(
+                        msg.channel, "Disabled plugin: **{}**".format(plugin)
+                    )
                 else:
-                    await self.send_message(msg.channel, "{} is already disabled".format(plugin))
+                    await self.send_message(
+                        msg.channel, "{} is already disabled".format(plugin)
+                    )
 
             elif action == "reload":
                 if plugins[plugin]['status'] == "Disabled":
                     self.core.plugin.reload(plugin)
-                    await self.send_message(msg.channel, "Reloaded plugin: **{}**".format(plugin))
+                    await self.send_message(
+                        msg.channel, "Reloaded plugin: **{}**".format(plugin)
+                    )
         else:
-            await self.send_message(msg.channel, "I don't have a plugin by that name.")
-
-    # @subscribe("connect")
-    # def on_connect(self, *args, **kwargs):
-    #     self.login_time = time.time()
+            await self.send_message(
+                msg.channel, "I don't have a plugin by that name."
+            )
 
     @command("^uptime$", access=ACCESS['uptime'], name='uptime',
-             doc_brief="`uptime`: prints the duration that I've been running for.")
+             doc_brief="`uptime`: prints the duration that "
+             "I've been running for.")
     async def uptime(self, msg, arguments):
         uptime = time.time() - self.login_time
 
@@ -201,11 +230,18 @@ class Manage(Plugin):
             minutes = int((uptime % (60 * 60)) / 60)
             seconds = int(uptime % 60)
 
-            if(days > 0):       readable += str(days) + " days "
-            if(hours > 0):      readable += str(hours) + " hours "
-            if(minutes > 0):    readable += str(minutes) + " minutes "
-            if(seconds > 0):    readable += str(seconds) + " seconds "
+            if (days > 0):
+                readable += str(days) + " days "
+            if (hours > 0):
+                readable += str(hours) + " hours "
+            if (minutes > 0):
+                readable += str(minutes) + " minutes "
+            if (seconds > 0):
+                readable += str(seconds) + " seconds "
 
             return readable
 
-        await self.send_message(msg.channel, "I've been connected for: **" + readable_time(uptime) + "**")
+        await self.send_message(
+            msg.channel,
+            "I've been connected for: **{}**".format(readable_time(uptime))
+        )
