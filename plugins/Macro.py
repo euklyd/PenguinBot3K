4805f57e3bcc37905ad0a3fd910abf38e0578c91
Euklyd <euklyd@users.noreply.github.com>
2017-02-07 20:01:04 -0800

    remove abusive full pasta form

--- plugins/Macro.py
+++ plugins/Macro.py
@@ -438,29 +438,24 @@ class Macro(Plugin):
     async def THE_PUNCH(self, msg, arguments):
         with open(macro_path.format("the_punch.json"), 'r') as punchfile:
             punch = json.load(punchfile)
-            if (arguments[0] is not None):
-                if (arguments[0] == '-h'):
-                    await self.send_message(
-                        msg.channel,
-                        ("Section keys are: {keys}\n"
-                         "Use with `{trigger}THE PUNCH <key>`").format(
-                            keys=", ".join(punch['dict']),
-                            trigger=self.core.default_trigger)
-                    )
-                elif (arguments[0] in punch['dict']):
-                    await self.send_message(
-                        msg.channel,
-                        punch['dict'][arguments[0]]
-                    )
-                else:
-                    await self.send_message(
-                        msg.channel,
-                        punch['dict']['the punch']
-                    )
+            if (arguments[0] is None or arguments[0] == '-h' or arguments[0] == 'help'):
+                await self.send_message(
+                    msg.channel,
+                    ("Section keys are: `{keys}`\n"
+                     "Use with `{trigger}THE PUNCH <key>`").format(
+                        keys="`, `".join(punch['dict']),
+                        trigger=self.core.default_trigger)
+                )
+            elif (arguments[0] in punch['dict']):
+                await self.send_message(
+                    msg.channel,
+                    punch['dict'][arguments[0]]
+                )
             else:
-                for meme in punch['array']:
-                    await self.send_message(msg.channel, meme)
-                    await asyncio.sleep(1)
+                await self.send_message(
+                    msg.channel,
+                    punch['dict']['the punch']
+                )
         ganon = self.core.emoji.emoji(msg.server, ['return_of_ganon'])
         if (ganon != "`:return_of_ganon:`"):
             await self.send_message(msg.channel, ganon)
