afd9e29145b9b178129210743e592e283283a5c0
Euklyd <euklyd@users.noreply.github.com>
2017-02-17 18:10:37 -0800

    nobody likes this

--- loop_bot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# loop_bot.sh
-
-python3.5 bot.py
-ERROR=$?
-while [[ ${ERROR} -ne 0 ]]; do
-  echo "[$(date "+%Y-%m-%d %H:%M:%S")] Bot exited with error code ${ERROR}; restarting."
-  python3.5 bot.py
-done
