8664b5b6fee4b5458638f3cdbb225d4e0dfa7552
Euklyd <euklyd@users.noreply.github.com>
2017-05-21 00:45:07 -0700

    update to be consistent with change to emojimanager

--- plugins/Nimious.py
+++ plugins/Nimious.py
@@ -39,11 +39,10 @@ class Nimious(Plugin):
     @command("^(?:[Nn]imious|<@!?163167281042423810>)$", access=100,
              name='Nimious')
     async def nimious(self, msg, arguments):
-        ww_link = self.core.emoji.emoji_str(['ww_link_ugh'])
-        bow = self.core.emoji.emoji_str(['bow'])
-        bomb = self.core.emoji.emoji_str(['bomb'])
-        danklink = self.core.emoji.emoji_str(['dank_link'])
-        # dank_meme = ("Shut the fuck up <:ww_link_ugh:230236082892046337> Nimious <:ww_link_ugh:230236082892046337>. You're :zzz: fraudulent. You can't do anything besides <:bomb:230238267067334656> spam <:bow:230238510169325580>. That's what you have :cancer: Rosa :star2: as a :second_place: secondary :second_place: because she complements your :cartwheel: roll habits :cartwheel: you cocky :toilet: ass low level smasher :toilet:. Gtfoh bruh. Your entire play style is :regional_indicator_z: 2 buttons :b:. You give Link mains a bad name. The reason no one cheers for you is because of the :cheese: fraudulence :cheese: you show in your play. A real Link main like :muscle: Izaw :muscle: gets my respect.\n<:dank_link:230168078980218881> You literally aren't shit dude <:dank_link:230168078980218881>")
+        ww_link = self.core.emoji.emoji_str(msg.server, ['ww_link_ugh'])
+        bow = self.core.emoji.emoji_str(msg.server, ['bow'])
+        bomb = self.core.emoji.emoji_str(msg.server, ['bomb'])
+        danklink = self.core.emoji.emoji_str(msg.server, ['dank_link'])
         dank_meme = (
             "Shut the fuck up {ww} Nimious {ww}. You're 💤 fraudulent. "
             "You can't do anything besides {bomb} spam {bow}. That's why you "
