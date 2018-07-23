62e1362186eef9ed6dc6fef76bfb839a3365b576
euklyd <euklyd@users.noreply.github.com>
2018-07-22 20:58:32 -0700

    initial commit of EiMM.py

---
+++ plugins/EiMM.py
@@ -0,0 +1,65 @@
+"""
+    Plugin Name : EiMM
+    Plugin Version : 0.1
+
+    Description:
+        Utilities for the Mafia-spinoff free-for-all game,
+        Everyone is Mafia Mafia.
+
+    Contributors:
+        - Euklyd / Popguin
+
+    License:
+        Arcbot is free software: you can redistribute it and/or modify it
+        under the terms of the GNU General Public License v3; as published
+        by the Free Software Foundation
+"""
+
+from core.Plugin import Plugin
+from core.Decorators import *
+
+import asyncio
+import discord
+import json
+import logging
+import random
+from io import BytesIO
+from PIL import Image
+
+logger = logging.getLogger(__name__)
+
+
+class EiMM(Plugin):
+    async def activate(self):
+        pass
+
+    def get_avatar_image(user):
+        response = requests.get(user.avatar_url)
+        return Image.open(BytesIO(response.content))
+
+    def create_dm_icon(icon1, icon2):
+        # Returns a 128x128, bytes-like representation of a combination of
+        # icon1 and icon2.
+        size = (128, 128)
+        i1 = icon1.resize(size)
+        i2 = icon2.resize(size)
+        i1 = i1.crop((0,0,64,128))
+        i2.paste(i1, (0,0))
+        i3 = BytesIO()
+        i2.save(i3, 'PNG')
+        i2.save('/tmp/dmicon.png', 'PNG')
+        return i3.getvalue()
+
+    @command("^[Dd][Mm]icon (\d+) (\d+)$", access=-1, name='DMicon',
+             doc_brief="`DMicon <userID>`: Creates an icon for a DM between yourself and another user.")
+    async def DMicon1(self, msg, arguments):
+        arguments.append(msg.author.id)
+        self.DMicon2(msg, arguments)
+
+    @command("^[Dd][Mm]icon (\d+) (\d+)$", access=-1, name='DMicon',
+             doc_brief="`DMicon <userID> <userID>`: Creates an icon for a DM between two users.")
+    async def DMicon2(self, msg, arguments):
+        u1 = await self.get_user_info(arguments[0])
+        u2 = await self.get_user_info(arguments[1])
+        icon = create_dm_icon(get_avatar_image(u1), get_avatar_image(u2))
+        await self.send_file(msg.channel, '/tmp/dmicon.png')
