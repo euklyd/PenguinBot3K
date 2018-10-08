a4d9b0e1e80826474157a53bb99a1473620a7421
euklyd <euklyd@users.noreply.github.com>
2018-10-08 01:49:34 -0700

    use game launch as timestamp

--- plugins/Macro.py
+++ plugins/Macro.py
@@ -845,7 +845,7 @@ class Macro(Plugin):
             color=msg.server.get_member(self.core.user.id).color,
             url="http://megamanspritecomic.tumblr.com/",
             description=quote['quote'],
-            timestamp=msg.timestamp
+            timestamp=datetime.utcfromtimestamp(1383363660)
         )
         em.set_thumbnail(url=icon_url)
         em.set_author(
